f5f3759b993e3e0dbd2e0c4c870095af576008fa
Onizuka09 <smokthar925@gmail.com>
2023-01-11 16:35:08 +0100

    initial commit

---
+++ test/test.ino
@@ -0,0 +1,20 @@
+ void setup (){
+
+Serial.begin(9600); 
+pinMode (13,OUTPUT); 
+
+}
+String a="";  
+void loop(){ 
+
+
+
+//Serial.print ("hello world "); 
+if (Serial.available ()){
+
+a = Serial.readStringUntil('\n');
+digitalWrite (13,!digitalRead(13)); 
+Serial.println(a);
+delay(1000);
+}
+}
